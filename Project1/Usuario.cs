6af9a52f5c397a47ab78e1515cff7afb46bba160
Julian Loaiza <julian.loaizal@udea.edu.co>
2022-01-20 11:13:00 -0500

    Actualizaciones

---
+++ Project1/Usuario.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Project1
+{
+    public class Usuario
+    {
+ 
+        public int usuId { get; set; }
+
+        public string nombre { get; set; }
+                
+        public string apellido { get; set; }
+    }
+}
